d47f5bf465440923830647885c3062d8dbe8a6d7
Jason Lin <uscjlin@gmail.com>
2016-07-16 15:47:55 -0700

    test purchase in test

--- src/etcjane/AviatoConn.java
+++ src/etcjane/AviatoConn.java
@@ -19,33 +19,38 @@ public class AviatoConn {
 		Date date = new Date();
 		 try
 	        {
-	            s = new Socket("test-exch-aviato", 20001);
+	            s = new Socket("test-exch-aviato", 20000);
 	            BufferedReader from_exchange = new BufferedReader(new InputStreamReader(s.getInputStream()));
 	            PrintWriter to_exchange = new PrintWriter(s.getOutputStream(), true);
 
 	            to_exchange.println("HELLO AVIATO");
+	            to_exchange.println("ADD 2 BOND BUY 998 5");
+	            to_exchange.println("ADD 3 BOND BUY 997 5");
+	            to_exchange.println("ADD 4 BOND BUY 995 5");
+	            to_exchange.println("ADD 5 BOND BUY 990 5");
+	            
 	            while((reply = from_exchange.readLine()) != null) {
 		            reply = reply.trim();
 //		            SimpleDateFormat sdf = new SimpleDateFormat("h:mm:ss:SSSSSS");
 //		            String formattedDate = sdf.format(date);
 		            long tmp = System.nanoTime();
-		            if (reply.startsWith("BOOK")) {
-		            	String[] splits = reply.split("BUY|SELL");
-		            	int i=0;
-		            	for (String str : splits) {
-		            		if (i == 1){
-		            			System.out.print(tmp + ": BUY ");
-		 
-		            		}
-		            		else if (i == 2) {
-		            			System.out.print(tmp + ": SELL ");
-		            		}
-		            		System.out.printf("%s\n", str);
-		            		i++;
-		            	}
-		            } else {
+//		            if (reply.startsWith("BOOK")) {
+//		            	String[] splits = reply.split("BUY|SELL");
+//		            	int i=0;
+//		            	for (String str : splits) {
+//		            		if (i == 1){
+//		            			System.out.print(tmp + ": BUY ");
+//		 
+//		            		}
+//		            		else if (i == 2) {
+//		            			System.out.print(tmp + ": SELL ");
+//		            		}
+//		            		System.out.printf("%s\n", str);
+//		            		i++;
+//		            	}
+//		            } else {
 		            	System.out.printf("%o: %s\n", tmp, reply);
-		            }
+//		            }
 		            System.out.println();
 	            }
 	        }
